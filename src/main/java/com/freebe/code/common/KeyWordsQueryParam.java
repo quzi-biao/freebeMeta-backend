2b6d985a2414df7191cec54226b3b0473ea26b31
ZHAO Hong <zhaohong@coderprepares.com>
2024-09-05 01:53:13 +0700

    task: 【前后端优化】任务广场增加搜索功能

--- src/main/java/com/freebe/code/common/KeyWordsQueryParam.java
+++ src/main/java/com/freebe/code/common/KeyWordsQueryParam.java
@@ -3,11 +3,14 @@ package com.freebe.code.common;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class KeyWordsQueryParam extends PageBean {
-	
 	@ApiModelProperty("关键词")
 	private String keyWords;
+
+	@ApiModelProperty("ID列表")
+	private List<?> idList;
 }
